500d0d412f2b63d9d6c669dffd53181b26f8242c
Karel Maesen <karel@geovise.com>
2021-05-16 13:46:39 +0200

    JPQL

--- web/src/main/java/com/example/repositories/TrajectoryCustomRepositoryImpl.java
+++ web/src/main/java/com/example/repositories/TrajectoryCustomRepositoryImpl.java
@@ -3,27 +3,12 @@ package com.example.repositories;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.ParameterExpression;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
-import org.hibernate.spatial.criterion.SpatialProjections;
-import org.hibernate.spatial.criterion.SpatialRestrictions;
-import org.hibernate.spatial.predicate.GeolatteFilterPredicate;
-import org.hibernate.spatial.predicate.GeolatteSpatialPredicates;
-import org.hibernate.spatial.predicate.JTSSpatialPredicates;
 
 import com.example.entities.Trajectory;
-import org.geolatte.geom.Envelope;
 import org.geolatte.geom.G2D;
-import org.geolatte.geom.Geometry;
 import org.geolatte.geom.Polygon;
 
 import static org.geolatte.geom.builder.DSL.g;
@@ -38,28 +23,39 @@ public class TrajectoryCustomRepositoryImpl implements TrajectoryCustomRepositor
 
 	@Override
 	public List<Trajectory> bbox(String bbox, int zoom) {
-		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-		CriteriaQuery<Trajectory> query = cb.createQuery( Trajectory.class );
-		Root<Trajectory> root = query.from( Trajectory.class );
-		Envelope<G2D> envelope = fromBbox( bbox );
-		GeolatteFilterPredicate filterPred = new GeolatteFilterPredicate( cb, root.get( "geometry" ), envelope );
-		query.where( filterPred );
-
-		return entityManager
-				.createQuery( query )
-				.getResultList();
+		TypedQuery<Trajectory> query = entityManager.createQuery(
+				"select t from Trajectory  t where filter(t.geometry, :param) = true ",
+				Trajectory.class
+		);
+		Polygon<G2D> filter = fromBbox( bbox );
+		query.setParameter( "param", filter );
+		return query.getResultList();
 	}
 
-	private Envelope<G2D> fromBbox(String bbox) {
+	private Polygon<G2D> fromBbox(String bbox) {
 		String[] elems = bbox.split( "," );
-		if (elems.length != 4) {
-			throw new IllegalArgumentException("Bbox requires exactly 4 doubles");
+		if ( elems.length != 4 ) {
+			throw new IllegalArgumentException( "Invalid BBox: expecting exactly 4 coordinates" );
 		}
 		try {
 			List<Double> co = Arrays.stream( elems ).map( Double::parseDouble ).collect( Collectors.toList() );
-			return new Envelope<>( co.get(0), co.get(1), co.get(2), co.get(3), WGS84 );
-		}catch(NumberFormatException e){
-			throw new IllegalArgumentException("Bbox requires exactly 4 doubles");
+			double minLon = co.get( 0 );
+			double minLat = co.get( 1 );
+			double maxLon = co.get( 2 );
+			double maxLat = co.get( 3 );
+			return polygon(
+					WGS84,
+					ring(
+							g( minLon, minLat ),
+							g( minLon, maxLat ),
+							g( maxLon, maxLat ),
+							g( maxLon, minLat ),
+							g( minLon, minLat )
+					)
+			);
+		}
+		catch (NumberFormatException e) {
+			throw new IllegalArgumentException( "Invalid bbox", e );
 		}
 
 	}
